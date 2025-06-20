00f783fbcacf95cfc089d5237bc47a42b1c4ef90
DanSivov <sivovolenkodaniil@gmail.com>
2025-06-20 21:25:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base : '/dmOctonus/',
   server: {
     host: "::",
     port: 8080,
